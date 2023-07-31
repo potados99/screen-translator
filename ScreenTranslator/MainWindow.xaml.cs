7563ea43b8775c15cbd0be8de969582a9c0334ff
potados99 <potados99@gmail.com>
2023-07-31 23:30:40 +0900

    chore: OCR 돌아감!

--- ScreenTranslator/MainWindow.xaml.cs
+++ ScreenTranslator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,20 +10,42 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.Globalization;
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace ScreenTranslator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary>  
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            DoSome();
+        }
+        
+        private async void DoSome()
+        {
+            var language = new Language("ko");
+            if (!OcrEngine.IsLanguageSupported(language))
+            {
+                throw new Exception($"{ language.LanguageTag } is not supported in this system.");
+            }
+            var stream = File.OpenRead("C:\\Users\\Administrator\\Desktop\\test.png");
+            var decoder = await BitmapDecoder.CreateAsync(stream.AsRandomAccessStream());
+            var bitmap = await decoder.GetSoftwareBitmapAsync();
+            var engine = OcrEngine.TryCreateFromLanguage(language);
+            var ocrResult = await engine.RecognizeAsync(bitmap).AsTask();
+
+            var a = ocrResult;
         }
     }
 }
