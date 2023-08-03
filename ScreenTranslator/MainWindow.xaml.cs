a0dacce28db6a47e92eb663779da27b0a5368b60
potados99 <potados99@gmail.com>
2023-08-03 20:52:31 +0900

    feat: draw ocr result on screen

--- ScreenTranslator/MainWindow.xaml.cs
+++ ScreenTranslator/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,6 +19,7 @@ using Windows.Graphics.Imaging;
 using Windows.Media.Ocr;
 using Windows.Storage;
 using Windows.Storage.Streams;
+using Color = System.Drawing.Color;
 
 namespace ScreenTranslator
 {
@@ -29,23 +32,60 @@ namespace ScreenTranslator
         {
             InitializeComponent();
 
-            DoSome();
+            Read().ContinueWith(_ => Draw());
         }
+
+        private OcrResult Result;
         
-        private async void DoSome()
+        private async Task<SoftwareBitmap> GetScreenshot()
+        {
+            var screenLeft = SystemParameters.VirtualScreenLeft;
+            var screenTop = SystemParameters.VirtualScreenTop;
+            var screenWidth = SystemParameters.VirtualScreenWidth;
+            var screenHeight = SystemParameters.VirtualScreenHeight;
+
+            using var bitmap = new Bitmap((int) screenWidth, (int) screenHeight);
+            using var g = Graphics.FromImage(bitmap);
+            g.CopyFromScreen((int) screenLeft, (int) screenTop, 0, 0, bitmap.Size);
+
+            using var stream = new InMemoryRandomAccessStream();
+            bitmap.Save(stream.AsStream(),
+                ImageFormat.Jpeg); //choose the specific image format by your own bitmap source
+            var decoder = await BitmapDecoder.CreateAsync(stream);
+            return await decoder.GetSoftwareBitmapAsync();
+        }
+
+        private async Task Read()
         {
             var language = new Language("ko");
             if (!OcrEngine.IsLanguageSupported(language))
             {
-                throw new Exception($"{ language.LanguageTag } is not supported in this system.");
+                throw new Exception($"{language.LanguageTag} is not supported in this system.");
             }
-            var stream = File.OpenRead("C:\\Users\\Administrator\\Desktop\\test.png");
-            var decoder = await BitmapDecoder.CreateAsync(stream.AsRandomAccessStream());
-            var bitmap = await decoder.GetSoftwareBitmapAsync();
+
+            var bitmap = await GetScreenshot();
             var engine = OcrEngine.TryCreateFromLanguage(language);
             var ocrResult = await engine.RecognizeAsync(bitmap).AsTask();
 
-            var a = ocrResult;
+            Result = ocrResult;
+        }
+
+        private void Draw()
+        {
+            var g = Graphics.FromHwnd(IntPtr.Zero);
+            foreach (var line in Result.Lines)
+            {
+                foreach (var word in line.Words)
+                {
+                    g.DrawString(
+                        word.Text,
+                        new Font("Arial", 14),
+                        new SolidBrush(Color.Yellow),
+                        (float)word.BoundingRect.X, 
+                        (float)word.BoundingRect.Y
+                    );
+                }
+            }
         }
     }
 }
